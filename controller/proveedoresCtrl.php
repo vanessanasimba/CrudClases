3782a377ffb7d1fc3644b711bf1c56cf4efb742d
vanessanasimba <nmvanessa25@gmail.com>
2024-08-08 20:16:10 -0500

    Creacion de proveedor angula

--- controller/proveedoresCtrl.php
+++ controller/proveedoresCtrl.php
@@ -1,6 +1,13 @@
 <?php
-require_once('../models/proveedores_model.php');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if($method == "OPTIONS") {die();}
 
+
+require_once('../models/proveedores_model.php');
 $proveedores = new Proveedores;
 
 switch ($_GET["op"]){
---
+++ tienda/src/app/Services/proveedor.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Iproveedor } from '../Interfaces/iproveedor';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProveedorService {
+  apiurl='http://localhost:8080/NasimbaVanessa_Tarea2/controller/proveedoresCtrl.php?op=';
+
+
+  constructor(private lector: HttpClient) { }
+
+  todos():Observable<Iproveedor[]>{
+
+    return this.lector.get<Iproveedor[]>(this.apiurl + 'todos');
+  }
+}
